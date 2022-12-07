b37977227b7126c39cfc24b68222fcca3f2dcd21
BB <boker.plus.bob@gmail.,com>
2022-12-07 16:41:07 +0200

    Updated folder path

--- CARDOC/Models/Vehicle.cs
+++ CARDOC/Models/Vehicle.cs
@@ -124,7 +124,7 @@ namespace CARDOC.Models
         {
             get
             {
-                return string.Format("{0}/{1} {2}", Const.ExportFolder, OutDate.ToString(Const.DateFormat), this.TemplateName);
+                return string.Format("{0}/{1} {2}", Const.ExportFolder, OutDate.ToString(Const.DateFormat), Unit);
             }
         }
     }
